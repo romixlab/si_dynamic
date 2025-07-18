f78e09f21ccb4d2db3a88bdccf02d13b10954fd5
Roman Isaikin <romix.lab@gmail.com>
2025-07-18 08:47:29 +0200

    feat: base and derived units, prefixes grammar.

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let x = "m/s²";
+        let x = "m/s⁻²";
+        let y = "m/s^2";
+        let z = "m/s^-2";
+    }
+}
